8a819611d647da6e8bb9eda3d8a87e49e110c6a9
Moe Poi ~ <moe@chocola.dev>
2023-10-25 17:25:36 +0700

    Add hover interaction to pause menu

--- scripts/ui/pause_menu.gd
+++ scripts/ui/pause_menu.gd
@@ -11,6 +11,18 @@ func hide_menu():
 func _on_resume_button_pressed():
 	on_resume.emit()
 
+func _on_resume_hold():
+	$Panel/MarginContainer/VBoxContainer/HBoxContainer/VBoxContainer/Resume.modulate = Color(0.541176, 0.541176, 0.541176, 1)
+
+func _on_resume_release():
+	$Panel/MarginContainer/VBoxContainer/HBoxContainer/VBoxContainer/Resume.modulate = Color(1, 1, 1, 1)
+
 func _on_main_menu_pressed():
 	on_resume.emit()
 	get_tree().change_scene_to_file("res://scenes/ui/main_menu.tscn")
+
+func _on_main_menu_hold():
+	$Panel/MarginContainer/VBoxContainer/HBoxContainer/VBoxContainer2/MainMenu.modulate = Color(0.541176, 0.541176, 0.541176, 1)
+
+func _on_main_menu_release():
+	$Panel/MarginContainer/VBoxContainer/HBoxContainer/VBoxContainer2/MainMenu.modulate = Color(1, 1, 1, 1)
