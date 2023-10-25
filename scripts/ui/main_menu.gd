16c0cc31d855bf3c81d1a88454344efab8d7f658
Moe Poi ~ <moe@chocola.dev>
2023-10-26 01:47:14 +0700

    Remove some dots from how to play

--- scripts/ui/main_menu.gd
+++ scripts/ui/main_menu.gd
@@ -3,6 +3,10 @@ extends Control
 
 var game_data = load("res://scripts/data/data.gd").new()
 
+@onready var timer = $Timer
+
+var debug_state = 0
+
 func _ready():
 	var data = game_data.load_data()
 	if data == null:
@@ -10,6 +14,21 @@ func _ready():
 			"debug": false,
 			"unlocked_stage": 1
 		})
+
+func _on_logo_pressed():
+	if timer.is_stopped():
+		timer.start()
+
+	debug_state += 1
+
+	if debug_state >= 6:
+		game_data.save_data({
+			"debug": true,
+			"unlocked_stage": 6
+		})
+
+func _on_timer_timeout():
+	debug_state = 0
 
 func _on_start_game_pressed():
 	get_tree().change_scene_to_file("res://scenes/ui/select_stage.tscn")
