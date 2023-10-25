b636c3933418049f3cbc00952a5c6bc087b79b4e
Moe Poi ~ <moe@chocola.dev>
2023-10-25 17:06:59 +0700

    Add hover interaction to placement

--- scripts/towers/placement/placement.gd
+++ scripts/towers/placement/placement.gd
@@ -7,3 +7,9 @@ func _on_input_event(_viewport, event, _shape_idx):
 	var mouse_click = event as InputEventMouseButton
 	if mouse_click and mouse_click.button_index == 1 and mouse_click.pressed:
 		on_placement.emit(global_position, get_instance_id())
+
+func _on_hold():
+	$Sprite2D.modulate = Color(0.243137, 0.243137, 0.243137, 1)
+
+func _on_release():
+	$Sprite2D.modulate = Color(1, 1, 1, 1)
