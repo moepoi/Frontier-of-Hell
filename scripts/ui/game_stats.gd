e811a33464c9762707b20c8d343b75adbc5b80f6
Moe Poi ~ <moe@chocola.dev>
2023-10-25 17:19:35 +0700

    Add hover interaction to pause button

--- scripts/ui/game_stats.gd
+++ scripts/ui/game_stats.gd
@@ -15,3 +15,9 @@ func set_damage(value):
 
 func _on_pause_button_pressed():
 	on_paused.emit()
+
+func _on_pause_button_hold():
+	$Pause/PauseButton.modulate = Color(0.615686, 0.615686, 0.615686, 1)
+
+func _on_pause_button_release():
+	$Pause/PauseButton.modulate = Color(1, 1, 1, 1)
